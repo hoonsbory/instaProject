3d83d14b6712302a1dac5da115fc97abc85568d9
DooWonJae <DooWonJae@172.30.1.43>
2019-09-25 14:47:19 +0900

    like

--- instaProject/src/service/LikesService.java
+++ instaProject/src/service/LikesService.java
@@ -2,9 +2,11 @@ package service;
 
 import java.util.List;
 
+import dao.LikesDAO;
 import vo.LikesVO;
 
 public interface LikesService {
-	int insertLikes(LikesVO vo);
-	int deleteLikes(int post_id);
+	int insertLikes(int user_id, int post_id) throws Exception;
+	int deleteLikes(int post_id, int user_id) throws Exception;
+	List<LikesVO> countLikes(int post_id) throws Exception;
 }
